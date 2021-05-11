09a433561c666ba4895b99ebeaa6579e804bee77
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-05-11 10:47:08 +0100

    Initial Commit

---
+++ Challenge day/Challenge_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_dayApp.swift
+//  Challenge day
+//
+//  Created by MAC on 11/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
